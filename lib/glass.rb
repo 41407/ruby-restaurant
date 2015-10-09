ba12f4a7ef34db044aceee8aedc2d1e43c76d7bd
leijona <dani.nummikoski@gmail.com>
2015-10-10 00:18:35 +0300

    Glass implemented

--- lib/glass.rb
+++ lib/glass.rb
@@ -1,4 +1,19 @@
 require "container"
 
 class Glass < Container
+
+	def initialize(capacity=0, weight=0, name="")
+		raise ArgumentError.new("weight must be greater than zero") unless weight >= 0
+		super(capacity, name)
+		@weight = weight
+	end
+
+	def weight
+		@weight
+	end
+
+	def fill
+		@weight = (@weight += @capacity).round(2)
+	end	
+
 end
