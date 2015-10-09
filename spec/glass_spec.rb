575b8e3c1dd9b23621a05e27a822925488acd8e4
41407 <jimi.welling@cs.helsinki.fi>
2015-10-09 23:19:05 +0300

    Add glass initializer spec cases

--- spec/glass_spec.rb
+++ spec/glass_spec.rb
@@ -3,9 +3,17 @@ require "glass"
 describe Glass do
   it "should return an empty glass with capacity, weight and name" do
   	pint = Glass.new(0.473, 0.2, "pint")
-  	expect(pint.capacity).to eq(1)
+  	expect(pint.capacity).to eq(0.473)
   	expect(pint.name).to eq("pint")
     expect(pint.weight).to eq(0.2)
+  end
+
+  it "should not have negative capacity" do
+    expect(Glass.new(-1)).to raise_error ArgumentError
+  end
+
+  it "should not have negative weight" do
+    expect(Glass.new(3, -2, "weird")).to raise_error ArgumentError
   end
 
   it "when full should return correct weight" do
